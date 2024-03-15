a5bc7a466b6783d7c1cce46a12509e904aadef79
Glaucus-M-Alm <glaucusmiral@gmail.com>
2024-03-15 14:52:50 -0300

    implementacao funcoes

--- matematica.c
+++ matematica.c
@@ -1,8 +1,14 @@
 #include "matematica.h"
 #include <math.h>
 //Calcula o seno do ângulo fornecido
-float calcular_seno(float angulo_radianos){}
+float calcular_seno(float angulo_radianos){
+    return sin(angulo_radianos);
+};
 //Calcula o cosseno do ângulo fornecido.
-float calcular_cosseno(float angulo_radianos);
+float calcular_cosseno(float angulo_radianos){
+    return cos(angulo_radianos);
+};
 //Calcula a tangente do ângulo fornecido.
-float calcular_tangente(float angulo_radianos);
+float calcular_tangente(float angulo_radianos){
+    return tan(angulo_radianos);
+};
