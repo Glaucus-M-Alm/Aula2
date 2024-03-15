41330673cd4376a9684129311a72fec23cb4be32
Glaucus <glaucusmiral@gmail.com>
2024-03-15 13:41:44 -0300

    commit inicial

---
+++ matematica.h
@@ -0,0 +1,12 @@
+#ifndef matematica_h
+#define matematica_h
+/*define e ifndef são postos por boas práticas para evitar problemas de inclusão múltipla
+para nosso problema não deve fazer muita diferença, mas é bom colocá-los
+*/
+float calcular_seno(float angulo_radianos); //Calcula o seno do ângulo fornecido.
+
+float calcular_cosseno(float angulo_radianos); //Calcula o cosseno do ângulo fornecido.
+
+float calcular_tangente(float angulo_radianos); //Calcula a tangente do ângulo fornecido.
+
+#endif
