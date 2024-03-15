ded6f329a999f9230b7914c5c33c5934f85ef681
Glaucus-M-Alm <glaucusmiral@gmail.com>
2024-03-15 14:47:46 -0300

    correcao das casas decimais

--- main.c
+++ main.c
@@ -5,9 +5,9 @@ int main() {
     float angulo_graus = 30.0;
     float angulo_radianos = angulo_graus * (M_PI / 180.0); // Convertendo para radianos usando M_PI
 
-    printf("Seno de %f graus: %f\n", angulo_graus, calcular_seno(angulo_radianos));
-    printf("Cosseno de %f graus: %f\n", angulo_graus, calcular_cosseno(angulo_radianos));
-    printf("Tangente de %f graus: %f\n", angulo_graus, calcular_tangente(angulo_radianos));
+    printf("Seno de %.3f graus: %.3f\n", angulo_graus, calcular_seno(angulo_radianos));
+    printf("Cosseno de %.3f graus: %.3f\n", angulo_graus, calcular_cosseno(angulo_radianos));
+    printf("Tangente de %.3f graus: %.3f\n", angulo_graus, calcular_tangente(angulo_radianos));
 
     return 0;
 }
